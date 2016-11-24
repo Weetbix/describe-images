b3d3fa4b11b3b670542dc9ce9536dd19a7eaa385
John Hannagan <weetbix_@hotmail.com>
2016-11-24 01:01:21 +0100

    Add ability to limit to 20 per minute

--- bin/run.js
+++ bin/run.js
@@ -7,9 +7,14 @@ program
   .option('-o, --output [output]', 'Filename to output the API responses in JSON format to, otherwise to console')
   .option('-i, --input <input>', 'Folder containing images to describe' )
   .option('-a, --apikey <apikey>', 'Microsoft Vision API Key')
+  .option('-l, --limit', 'Limit requests to 20 per minute')
   .parse(process.argv);
 
 const IS_IMAGE = /\.(jpe?g|png|gif|bmp)$/i;
+
+function timeout(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
 
 (async function run()
 {
@@ -17,7 +22,42 @@ const IS_IMAGE = /\.(jpe?g|png|gif|bmp)$/i;
                    .filter(filename => IS_IMAGE.test(filename))
                    .map(filename => path.join(program.input,filename));
 
-    let descriptions = await lib.describeImages( images, program.apikey );
+    console.log(`fetching ${images.length} image descriptions`);
 
-    console.log( JSON.stringify(descriptions, null, 2) );
+    let descriptions;
+    if(!program.limit)
+    {
+        descriptions = await lib.describeImages( images, program.apikey );
+    }
+    else
+    {
+        // Split array into chunks of 20
+        descriptions = { descriptions : [], errors : [] };
+
+        const CHUNK_SIZE = 1;
+        const ONE_MINUTE = 60000;
+        var i, j, imageChunk;
+        for(i=0, j=images.length; i < j; i += CHUNK_SIZE)
+        {
+            console.log(`Requesting ${CHUNK_SIZE} image descriptions`);
+            imageChunk = images.slice(i, i + CHUNK_SIZE);
+
+            var descriptionChunk = await Promise.all([
+                lib.describeImages(imageChunk, program.apikey),
+                timeout(ONE_MINUTE)
+            ]);
+
+            descriptions.descriptions.push(
+                ...descriptionChunk[0].descriptions
+            );
+            descriptions.errors.push(
+                ...descriptionChunk[0].errors
+            );
+        }
+    }
+
+    const output = JSON.stringify(descriptions, null, 4);
+    fs.writeFileSync(program.output, output);
+
+    console.log( `Finished with ${descriptions.errors.length} bad requests`);
 }());
